d0935a9f668be8e89ecca39ecb879f57d14b6073
David James <davidcjames@gmail.com>
2023-09-28 11:50:07 -0400

    Explain purpose of tests

--- tests/macro.rs
+++ tests/macro.rs
@@ -1,7 +1,8 @@
 use q_num::define_q_num;
 
+/// Spot check the macro-generated constants for `Q12.5`.
 #[test]
-fn test_macro_q12p5() {
+fn test_q12p5_constants() {
     define_q_num!(X, Q12.5);
     assert_eq!(X::TOTAL_BITS, 32);
     assert_eq!(std::mem::size_of::<X>(), 4);
@@ -16,8 +17,9 @@ fn test_macro_q12p5() {
     assert_eq!(X::MAX_FLOAT, 2047.96875);
 }
 
+/// Spot check addition for `Q12.5`.
 #[test]
-fn test_macro_add() {
+fn test_q12p5_add() {
     define_q_num!(X, Q12.5);
     let x1 = X::from(500.25);
     let x2 = X::from(-744.75);
@@ -29,20 +31,41 @@ fn test_macro_add() {
     assert_eq!(x1 + x2, x3)
 }
 
+/// Test that the `define_q_num` macro is hygienic.
+/// 
+/// - If the macro is non-hygienic, it may refer to an unqualified `u8`. This is
+///   a problem because `u8` could be shadowed. In the test below, it is
+///   shadowed to refer to `u16`, which would result in an incorrect size of 2
+///   bytes.
+/// - If the macro is hygienic, it will correctly refer to the full qualified
+///   type: `::core::primitive::u8`.
 #[test]
-fn test_shadowed_type() {
+fn test_q4p2_hygienic_inner_type() {
     #[allow(non_camel_case_types)]
     #[allow(dead_code)]
     type u8 = u16;
-    // If the macro is non-hygienic, it will use the shadowed type.
-    // If the macro is hygienic, it will use `::core::primitive::u8`.
     define_q_num!(X, Q4.2);
     // The correct size is 1 bytes = 8 bits.
     assert_eq!(std::mem::size_of::<X>(), 1);
 }
 
+/// Exhaustively test all valid inner values for `UQ5.3`.
 #[test]
-fn test_q4p2() {
+fn test_q5p3_inner_values() {
+    define_q_num!(Q, UQ5.3);
+    let mut value = 0.0;
+    for byte in 0..0b1111_1111 {
+        let q = Q::from(value);
+        assert_eq!(q.to_bits(), byte);
+        value += 0.125;
+    }
+}
+
+/// Exhaustively test all valid inner values for `UQ4.2`. In this case where
+/// only 6 of 8 bits are used, the inner values are left-aligned, leaving the
+/// rightmost 2 bits unused.
+#[test]
+fn test_q4p2_inner_values() {
     define_q_num!(Q, UQ4.2);
     let mut value = 0.0;
     for byte in (0..0b1111_1100).step_by(0b100) {
@@ -52,20 +75,10 @@ fn test_q4p2() {
     }
 }
 
+/// Spot check an out-of-range input for `Q4.2`.
 #[test]
 #[should_panic]
 fn test_q4p2_out_of_range() {
     define_q_num!(Q, Q4.2);
     let _ = Q::from(16.00);
-}
-
-#[test]
-fn test_q5p3() {
-    define_q_num!(Q, UQ5.3);
-    let mut value = 0.0;
-    for byte in 0..0b1111_1111 {
-        let q = Q::from(value);
-        assert_eq!(q.to_bits(), byte);
-        value += 0.125;
-    }
 }
