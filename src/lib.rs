cc3cf1dd52b7fb2647be72d162ee3c116174c36b
David James <xpe@ixm.local>
2024-04-28 07:29:07 -0400

    adjust doc in lib.rs to match README style

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,8 @@
 //! compile time) to define a signed/unsigned binary fixed-point number type. It
 //! uses ARM-style Q notation: `Qm.n` (signed) or `UQm.n` (unsigned), where:
 //!
-//! - `m` is the number of integer bits, and
-//! - `n` is the number of fractional bits.
+//! - **m** is the number of integer bits, and
+//! - **n** is the number of fractional bits.
 //!
 //! Internally, the macro chooses the narrowest primitive integer type that can
 //! hold `m + n` bits, up to `u64` (unsigned) and `i64` (signed). More internal
@@ -18,10 +18,10 @@
 //!
 //! ## Q Number Value
 //!
-//! A Q number's value is the ratio of the stored number (having `n + m` bits)
-//! and a fixed denominator (equal to `2 ^ n`).
+//! A Q number's value is the ratio of the stored number (having n + m bits)
+//! and a fixed denominator (equal to 2 ^ n).
 //!
-//! For example, using the UQ3.2 specification, the bit pattern `0b10111`
+//! For example, using the UQ3.2 specification, the bit pattern 0b10111
 //! represents the value 5.75. Keeping in mind the denominator is 2 ^ 2 = 4,
 //! there are two ways to see this:
 //!
